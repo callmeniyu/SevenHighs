4409abc4e4127012cd250f1c9b35537f9b2feb93
callmeniyu <getniyashere@gmail.com>
2025-03-02 22:34:30 +0530

    resolve issue blog not showing in the inventory

--- app/components/Inventory.tsx
+++ app/components/Inventory.tsx
@@ -1,67 +1,67 @@
 "use client"
+import { useEffect, useState } from "react"
 import Image from "next/image"
+import Link from "next/link"
 import { BlogType } from "../types"
 import AdminSearch from "./ui/AdminSearch"
-import { useEffect, useState } from "react"
 import { parseDate } from "../lib/utils"
-import Link from "next/link"
+import { collection, getDocs, orderBy, query } from "firebase/firestore"
+import { database } from "@/firebaseConfig"
 
-
-const Inventory = ({ allBlogs }: { allBlogs: BlogType[] }) => {
+const Inventory = () => {
+    const [blogs, setBlogs] = useState<BlogType[]>([])
     const [searchedBlogs, setSearchedBlogs] = useState<BlogType[]>([])
     const [sortBy, setSortBy] = useState<string>("blogNo")
-    const [sortedBlogs, setSortedBlogs] = useState<BlogType[]>(allBlogs)
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            const blogsRef = collection(database, "blogs")
+            const q = query(blogsRef, orderBy("blogNo", "asc"))
+            const allBlogsSnapshot = await getDocs(q)
+            const blogsData: BlogType[] = allBlogsSnapshot.docs.map((doc) => doc.data() as BlogType)
+            setBlogs(blogsData)
+        }
+        
+        fetchBlogs()
+    }, []) // Fetch data when component mounts
+
     const findBlog = (searchResult: BlogType[]) => {
         setSearchedBlogs(searchResult)
-        if (searchResult.length > 0) console.log("SearchedBlogs", searchResult)
     }
 
     const handleSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setSortBy(e.target.value)
     }
-    useEffect(() => {
-        let blogsToSort = searchedBlogs ? searchedBlogs:[...allBlogs];
-        const sortBlogs = () => {
-            if (sortBy == "date") {
-                const blogsByDate = blogsToSort.sort((a, b) => parseDate(b.date).getTime() - parseDate(a.date).getTime())
-                setSortedBlogs(blogsByDate)
-            }
-            else if (sortBy == "views") {
-                const blogsByViews = blogsToSort.sort((a, b) => b.views - a.views)
-                setSortedBlogs(blogsByViews)
-            } else {
-                setSortedBlogs(allBlogs);
-            }
-        }
-        sortBlogs()
-    }, [sortBy, allBlogs, sortBy])
+
+    const sortedBlogs = [...(searchedBlogs.length > 0 ? searchedBlogs : blogs)].sort((a, b) => {
+        if (sortBy === "date") return parseDate(b.date).getTime() - parseDate(a.date).getTime()
+        if (sortBy === "views") return b.views - a.views
+        return a.blogNo - b.blogNo
+    })
 
     return (
-        <div className="bg-primary-dark flex flex-col gap-4 items-center p-3  rounded-lg ">
+        <div className="bg-primary-dark flex flex-col gap-4 items-center p-3 rounded-lg">
             <div className="flex gap-4 justify-center my-3 items-center">
                 <select
-                    className="text-black flex gap-1 items-center border border-white p-2 px-3 rounded-md"
+                    className="text-black border border-white p-2 px-3 rounded-md"
                     value={sortBy}
-                    onChange={(e) => handleSort(e)}
+                    onChange={handleSort}
                 >
                     <option value="blogNo">Blog No</option>
                     <option value="date">Date</option>
                     <option value="views">Views</option>
-                    {/* <MdKeyboardArrowDown className="text-2xl" /> */}
                 </select>
-                <AdminSearch allBlogs={allBlogs} findBlogs={findBlog} />
+                <AdminSearch allBlogs={blogs} findBlogs={findBlog} />
             </div>
             <hr className="w-full" />
-
-            <div className="flex gap-2 md:px-2 justify-between w-full text-white font-semibold ">
+            <div className="flex gap-2 md:px-2 justify-between w-full text-white font-semibold">
                 <h6 className="w-2/10">Id</h6>
                 <h6 className="w-2/10">Category</h6>
                 <h6 className="w-2/10">Views</h6>
                 <h6 className="w-2/10">Section</h6>
                 <h6 className="w-2/10">Image</h6>
             </div>
-
-            {(searchedBlogs.length > 0 ? searchedBlogs : sortedBlogs).map((blog: BlogType, index: number) => (
+            {sortedBlogs.map((blog, index) => (
                 <Link href={`/admin/create/${blog.id}`} className="flex flex-col gap-2 w-full bg-secondary-light rounded-lg p-2 md:px-3" key={index}>
                     <h2 className="font-semibold">{blog.title}</h2>
                     <div className="flex w-full justify-between">
